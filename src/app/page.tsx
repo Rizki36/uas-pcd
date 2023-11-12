664c498dc19f08bcdd933049e4cfc98e7e65743b
Fitra R <cahrizki.rf@gmail.com>
2023-11-12 17:21:47 +0700

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main>{/*  */}</main>;
+  return (
+    <main className="flex items-center justify-center h-full">
+      <div>
+        <div className="text-lg">Hanya Tugas UAS Pengolahan Citra Digital</div>
+        <div className="text-center text-sm">
+          <a href="https://github.com/Rizki36/uas-pcd">
+            🚀🚀🚀 Source Code🚀🚀🚀
+          </a>
+        </div>
+      </div>
+    </main>
+  );
 }
